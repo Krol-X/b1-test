1dff5e9af1381ecde5b6cd1272f67880ccffecd6
Alex Krol <krolmail@list.ru>
2024-03-31 00:49:23 +0300

    Delete unecessary migration, config app port

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,14 @@ namespace App\Providers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider {
   /**
    * Register any application services.
    */
   public function register(): void {
-    //
+    Sanctum::ignoreMigrations();
   }
 
   /**
